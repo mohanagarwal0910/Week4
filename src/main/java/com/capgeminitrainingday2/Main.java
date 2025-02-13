07ab427434e487ebe4c75b5b737dd51730b0184e
mohanagarwal0910 <159651002+mohanagarwal0910@users.noreply.github.com>
2025-02-13 11:36:05 +0530

    [Mohan]add.Main.java

--- src/main/java/com/capgeminitrainingday2/Main.java
+++ src/main/java/com/capgeminitrainingday2/Main.java
@@ -5,12 +5,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list=new ArrayList<>();
-        list.add(9);
-        list.add(2);
-        list.add(29);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println("Hello world!");
+
     }
 }
