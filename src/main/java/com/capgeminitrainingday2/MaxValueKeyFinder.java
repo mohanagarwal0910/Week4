886a66ea9d463a257cbe5c4c00b07c133f9118c5
mohanagarwal0910 <159651002+mohanagarwal0910@users.noreply.github.com>
2025-02-13 11:44:07 +0530

    [Mohan]add.MaxValueKeyFinder.java

--- src/main/java/com/capgeminitrainingday2/MaxValueKeyFinder.java
+++ src/main/java/com/capgeminitrainingday2/MaxValueKeyFinder.java
@@ -9,6 +9,7 @@ public class MaxValueKeyFinder {
         }
         return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
+    
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
         map.put("A", 10);
